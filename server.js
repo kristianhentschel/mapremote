a168f9492dbdd7c21647aa39d8e5570dec66cbe9
Kristian Hentschel <khentschel@gmail.com>
2013-09-08 15:49:10 +0200

    simpler, much simpler session handling making use of function namespaces rather than big global objects.

--- server.js
+++ server.js
@@ -4,23 +4,67 @@ var app = require('http').createServer(handler)
 
 app.listen(80);
 
-var clients  = new Array();
-var sessions = new Array();
+
+// application code
+openSessions = new Array();
+
+io.sockets.on('connection', function (socket) {
+  
+  // create an open session for this client
+  // TODO: generate a random four-digit code that has not been used before. (allocate/free semantics?)
+  session = {
+    name: openSessions.length,
+    host: socket,
+    controller: null
+  };
+  openSessions[session.name] = session;
+
+  socket.set('sessionName', session.name, function() {
+    // Return the allocated session name for display on the client.
+    socket.emit('id', {sessionName: session.name});
+
+    // The joinsession event is emitted by the controller client when the user types a session code.
+    socket.on('joinsession', function (data) {
+      // find session and its host socket  
+      if (openSessions[data.sessionName] !== undefined && openSessions[data.sessionName] !== null) {
+        // session is not open anymore - keep local copy and remove from array.
+        session = openSessions[data.sessionName];
+        session.controller = socket;
+        openSessions[data.sessionName] = null;
+        
+        // Start Controller client, forwarding events from it to Host client
+        // Stop listening for join requests from it, and remove the unused session allocated.
+        session.controller
+          .removeAllListeners('joinsession')
+          .emit('startsession', {sessionName: session.name})
+          .get('sessionName', function(sessionName) {
+            // Remove open session for client, since this will never be used.
+            // TODO return sessionName to store of available names
+            openSessions[sessionName] = null;
+          })
+          .on('update', function (data) {
+            // TODO: based on eventName, send volatile or nonvolatile event.
+            // TODO: use different socket.io events rather than encapsulating everything in 'update'.
+            session.host.emit('update', {updateData: data.updateData});
+          });
+
+        // Start Host client
+        // and stop listening for join requests from it.
+        session.host
+          .removeAllListeners('joinsession')
+          .emit('startsession', {sessionName:session.name})
+      } else {
+        // session id not found 
+        console.log("client provided unknown session id");
+        socket.emit('error', {msg: "Unknown Session Name"});
+      }
+    });
+  });
+});
 
 
-function updateSession(socket, data) {
-  if (sessions[data.sessionName]) {
-    session = sessions[data.sessionName];
-    console.log("updating session with "+session.clients.length+" clients.");
-    for (i = 0; i < session.clients.length; i++) {
-      client = session.clients[i];
-      client.emit("update", {updateData: data.updateData});
-    }
-  } else {
-    socket.emit("error", {msg: "Invalid Session"});
-  }
-}
 
+// basic http handler to server static content.
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
   function (err, data) {
@@ -34,39 +78,3 @@ function handler (req, res) {
   });
 }
 
-io.sockets.on('connection', function (socket) {
-  id = clients.length;
-  console.log(id);
-  clients[id] = socket;
-  socket.set("clientId", id, function() {
-    sessionName = id;
-
-    sessions[sessions.length] = {
-      sessionName: sessionName,
-      clients: [socket]
-    };
-
-    socket.get('clientId', function(err, clientId) {
-      console.log("new client connected, giving clientId "+clientId+" and sessionName "+sessionName+".");
-      socket.emit('id', {clientId: clientId, sessionName: sessionName});
-
-      socket.on('joinsession', function (data) {
-        console.log(data.clientId + " joining session " + data.sessionName);
-        if (sessions[data.sessionName] !== undefined) {
-          session = sessions[data.sessionName];
-          session.clients[session.clients.length] = socket;
-          console.log("joining existing session");
-          for (i = 0; i < session.clients.length; i++) {
-            session.clients[i]
-              .emit('startsession', {sessionName: data.sessionName})
-              .on('update', function (data) {
-                updateSession(socket, data);
-              });
-          }
-        } else {
-          socket.emit('error', {msg: "Unknown Session Name"});
-        }
-      });
-    });
-  }); 
-});
